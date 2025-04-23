cb3f605075c5a574b5e221e9307c70a791728966
Henrique Pereira <rick.morey.pereira@gmail.com>
2025-04-23 13:02:35 -0300

    Commit testando o verified!

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello World!");
-        System.out.print("Testando se vai aparecer algo no git status");
+        System.out.println("Testando se vai aparecer algo no git status");
+        System.out.println("Finalmente um commit verificado!!");
     }
 }
